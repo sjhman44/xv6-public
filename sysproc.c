1b8830eb722f29f204b0d95d852fb5b4a520a9a6
sjhman44 <stephenhess44@gmail.com>
2019-03-17 04:11:25 +0000

    Added wcupa

--- sysproc.c
+++ sysproc.c
@@ -89,3 +89,9 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+int
+sys_wcupa(void)
+{
+	return 1871;
+}
---
+++ wcupa.c
@@ -0,0 +1,8 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int main (void){
+	printf(1,"West Chester Univeristy was founded in %d\n",wcupa());
+	exit();
+}
